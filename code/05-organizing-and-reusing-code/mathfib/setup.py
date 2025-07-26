cc78582f4ace1aa4a25aa57b7c9ff12867200650
Michael Kennedy <mikeckennedy@gmail.com>
2025-07-26 11:14:35 -0700

    First steps at creating a python package.

--- code/05-organizing-and-reusing-code/mathfib/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="mathfib",
-    version="0.1.0",
-    packages=find_packages(where=".", include=["mathfib*"]),  # Include both mathfib and mathfib.mathfib
-)
